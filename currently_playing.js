b7ee5e5924c14a23fa3f284ae7762f85c522a72f
Evgeny <EvgenyLavrov@gmail.com>
2020-07-15 10:21:21 +0300

    Add files via upload

---
+++ currently_playing.js
@@ -0,0 +1,20 @@
+function updateCP() {
+    var div_cp = document.getElementById('cp');//The HTML document that uses this script must have a div tag with the id 'cp'.
+    if(typeof elem !== null && elem !== 'undefined' ) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('GET', 'http://192.168.1.2:8000/', true);//icecast broadcast address
+        xmlhttp.onreadystatechange = function(){
+            if(xmlhttp.readyState == 4) {
+                if(xmlhttp.status == 200) {
+                    var elems = xmlhttp.responseXML.getElementsByClassName('streamstats');
+                    var str = '';
+                    Array.prototype.forEach.call(elems, function(el) {
+                        str = el.innerText;
+                    });
+                    div_cp.innerHTML = '<p>Currently playing: ' + str + '</p>';
+                }
+			}
+		}
+		xmlhttp.send(null);
+	}
+}
