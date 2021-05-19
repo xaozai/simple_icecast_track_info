4b9af9ca28dcb4717f56f4f27020c24640f398df
Evgeny <EvgenyLavrov@gmail.com>
2021-05-19 16:31:34 +0300

    Update currently_playing.js
    correction

--- currently_playing.js
+++ currently_playing.js
@@ -1,20 +1,16 @@
 function updateCP() {
     var div_cp = document.getElementById('cp');//A HTML document that uses this script must have a div tag with the id 'cp'.
-    if(typeof elem !== null && elem !== 'undefined' ) {
+    if(typeof div_cp !== null && div_cp !== 'undefined' ) {
         var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open('GET', 'http://192.168.1.2:8000/', true);//icecast broadcast address
+        xmlhttp.open('GET', 'http://192.168.1.2:8000/status-json.xsl', true);//icecast broadcast address
+        xmlhttp.responseType = 'json';
         xmlhttp.onreadystatechange = function(){
             if(xmlhttp.readyState == 4) {
                 if(xmlhttp.status == 200) {
-                    var elems = xmlhttp.responseXML.getElementsByClassName('streamstats');
-                    var str = '';
-                    Array.prototype.forEach.call(elems, function(el) {
-                        str = el.innerText;
-                    });
-                    div_cp.innerHTML = '<p>Currently playing: ' + str + '</p>';
+                    div_cp.innerHTML = '<p>Currently playing: ' + xmlhttp.response.icestats.source[1].title + '</p>';//[1] replace with your source index
                 }
-	    }
-	}
-	xmlhttp.send(null);
+            }
+        }
+        xmlhttp.send(null);
     }
 }
