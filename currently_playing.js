6ea93f8116e60fa1f1530be2f6c89088f079bd5d
Evgeny <EvgenyLavrov@gmail.com>
2021-05-24 18:18:17 +0300

    Update currently_playing.js
    Working with XMLHttpRequest is placed in a separate function. Added JS Promise, added data retrieval error handling.

--- currently_playing.js
+++ currently_playing.js
@@ -1,16 +1,30 @@
+function getJsonData(url) {
+    return new Promise((resolve, reject) => {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('GET', url, true);
+        xmlhttp.responseType = 'json';
+        xmlhttp.send(null);
+        xmlhttp.addEventListener('error', () => reject(xmlhttp.statusText));
+        xmlhttp.addEventListener('abort', () => reject(xmlhttp.statusText));
+        xmlhttp.addEventListener('load', () => {
+            if (xmlhttp.status == 200) {
+                resolve(xmlhttp.response);
+            } else {
+                reject(xmlhttp.statusText);
+            }
+        });
+    });
+}
+
 function updateCP() {
     var div_cp = document.getElementById('cp');//A HTML document that uses this script must have a div tag with the id 'cp'.
-    if(typeof div_cp !== null && div_cp !== 'undefined' ) {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open('GET', 'http://192.168.1.2:8000/status-json.xsl', true);//icecast broadcast address (http://194.116.73.162/status-json.xsl for example)
-        xmlhttp.responseType = 'json';
-        xmlhttp.onreadystatechange = function(){
-            if(xmlhttp.readyState == 4) {
-                if(xmlhttp.status == 200) {
-                    div_cp.innerHTML = '<p>Currently playing: ' + xmlhttp.response.icestats.source[0].title + '</p>';//[0] replace with your source index
-                }
-            }
-        }
-        xmlhttp.send(null);
+    if (typeof div_cp !== null && div_cp !== 'undefined') {
+        getJsonData('http://192.168.1.2:8000/status-json.xsl')//icecast broadcast address with status-json.xsl (http://194.116.73.162/status-json.xsl for example)
+            .then((jsonData) => {
+                div_cp.innerHTML = '<p>Currently playing: ' + jsonData.icestats.source[0].title + '</p>';//[0] replace with your source index
+            })
+            .catch((reasonText) => {
+                div_cp.innerHTML = `<p>Currently playing: <font color="#ffb6c1">Error!</font> Unable to load data (${reasonText}).</p>`;
+            });
     }
 }
